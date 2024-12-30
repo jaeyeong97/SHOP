352fb9e35a5bdd013e6cfdacfd26d9b44312f670
jaeyeong97 <rkaakdbdb1822@naver.com>
2024-12-30 12:27:02 +0900

    이미지 파일 크기 줄임

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import { lazy, useEffect } from "react";
+import { lazy } from "react";
 
 const KakaoLogin = lazy(() => import("../login/KakaoLogin"));
 const GoogleLoginComponent = lazy(() => import("../login/GoogleLoginComponent"));
@@ -27,23 +27,6 @@ const LoginPage = () => {
     sessionStorage.setItem("profileImg", naverAccount.profileImg);
     navigate("/");
   };
-
-  useEffect(() => {
-    const sdkUrls = [
-      "https://developers.kakao.com/sdk/js/kakao.js",
-      "https://accounts.google.com/gsi/client",
-      "https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js",
-    ];
-
-    sdkUrls.forEach((url) => {
-      if (!document.querySelector(`script[src="${url}"]`)) {
-        const script = document.createElement("script");
-        script.src = url;
-        script.async = true;
-        document.body.appendChild(script);
-      }
-    });
-  }, []);
 
   return (
     <section id="login-page">
